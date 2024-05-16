b7b0ed16d90fb30bdf5cb39b6d586d3eeafe45ff
programadoragain <ferdandi04@gmail.com>
2024-05-16 18:44:38 -0300

    new contact toggleModal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { getContact } from './api/ContactService';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  const modalRef  = useRef();
   const [data, setData] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
 
@@ -21,7 +22,7 @@ function App() {
     }
   }
 
-  const toogleModal = (show) => { console.log('I was click'); };
+  const toogleModal = (show) => { show ? modalRef.current.showModal() : modalRef.current.close() };
 
   useEffect(() => {
     getAllContacts();
@@ -38,6 +39,54 @@ function App() {
           </Routes>
         </div>
       </main>
+
+      {/* Modal */}
+      <dialog ref={modalRef} className="modal" id="modal">
+        <div className="modal__header">
+          <h3>New Contact</h3>
+          <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+        </div>
+        <div className="divider"></div>
+        <div className="modal__body">
+          <form onSubmit={handleNewContact}>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Name</span>
+                <input type="text" value={values.name} onChange={onChange} name='name' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input type="text" value={values.email} onChange={onChange} name='email' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Title</span>
+                <input type="text" value={values.title} onChange={onChange} name='title' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input type="text" value={values.phone} onChange={onChange} name='phone' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Address</span>
+                <input type="text" value={values.address} onChange={onChange} name='address' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Account Status</span>
+                <input type="text" value={values.status} onChange={onChange} name='status' required />
+              </div>
+              <div className="file-input">
+                <span className="details">Profile Photo</span>
+                <input type="file" onChange={(event) => setFile(event.target.files[0])} ref={fileRef} name='photo' required />
+              </div>
+            </div>
+            <div className="form_footer">
+              <button onClick={() => toggleModal(false)} type='button' className="btn btn-danger">Cancel</button>
+              <button type='submit' className="btn">Save</button>
+            </div>
+          </form>
+        </div>
+      </dialog>
+
     </>
   );
 }
